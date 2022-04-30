c2cede0fa4efab50d24e03154ccb3acc31b8780e
parseb <petra306@protonmail.com>
2022-04-30 21:17:43 +0200

    miniVest simple vesting contract

--- src/Vestoor.sol
+++ src/Vestoor.sol
@@ -3,14 +3,19 @@ pragma solidity 0.8.11;
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-
+// import "./test/utils/Console2.sol";
 /// @title Vestoor
-/// @author parseb petra306@protonmail.com
+/// @author parseb | @parseb | petra306@protonmail.com
+/// @notice Minimalist vesting contract exercise
+/// @dev As expected. Experimental
+/// @custom:security contact: petra306@protonmail.com
+
 contract Vestoor is ReentrancyGuard {
 
     /// @notice storage and getter for vesting agreements
     mapping (address => mapping(address => uint256)) public vestings;
     uint256 immutable k; //19
+    uint256 immutable oneToken; //1e18
 
     error VestingInProgress(address, address);
 
@@ -22,6 +27,7 @@ contract Vestoor is ReentrancyGuard {
     /// @param _k constant for vesting time and ammount encoding in 1 uint256
     constructor(uint256 _k) public {
         k = _k;
+        oneToken = 1e18;
     }
 
 
@@ -29,24 +35,23 @@ contract Vestoor is ReentrancyGuard {
     function setVest(address _token, 
                     address _beneficiary, 
                     uint256 _amount, 
-                    uint256 _enddate) // days from now
+                    uint256 _days) // days from now
                     external 
                     returns (bool s) {
 
         if (vestings[_token][_beneficiary] != 0) revert VestingInProgress(_token, _beneficiary);
-        require(IERC20(_token).balanceOf(msg.sender) >= _amount, "Insufficient funds");
+        require(IERC20(_token).balanceOf(msg.sender) >= _amount * oneToken, "Insufficient funds");
 
-        require(_amount > 0, "Amount must be greater than 0");
+        require(_amount * _days > 1, "Amount must be greater than 0");
         require(_beneficiary != address(0), "Beneficiary is 0");
-        require(_enddate > block.timestamp, "Present given. Requires Future.");
-        require(_amount > _enddate, "Amount must be greater than enddate");
+        require(_amount > _days, "Amount must be greater than days");
         require(_amount < k, "Max amount is k-1");
 
-        vestings[_token][_beneficiary] = _amount * k + ( _enddate * 1 days + block.timestamp );
+        vestings[_token][_beneficiary] = _amount * k + ( _days * 1 days + block.timestamp );
 
-        s = IERC20(_token).transferFrom(msg.sender, address(this), _amount);
+        s = IERC20(_token).transferFrom(msg.sender, address(this), _amount * oneToken);
 
-        emit NewVesting(_token, _beneficiary, _amount, _enddate);
+        emit NewVesting(_token, _beneficiary, _amount, _days);
         return s;
     }
 
@@ -56,8 +61,8 @@ contract Vestoor is ReentrancyGuard {
         uint256 iv= vestings[_token][msg.sender];
         require(vestings[_token][msg.sender] != 0, "Nothing to bag");
 
-        if (iv % k > block.timestamp) {
-            s = IERC20(_token).transfer(msg.sender, iv/k);
+        if (iv % k < block.timestamp) {
+            s = IERC20(_token).transfer(msg.sender, (iv/k) * oneToken);
             require(s, "Transfer failed");
             vestings[_token][msg.sender] = 0;
 
@@ -65,12 +70,18 @@ contract Vestoor is ReentrancyGuard {
 
         } else {
             uint256 eligibleAmount = (iv / k) - ( ((iv / k) / (iv % k)) * ( (iv % k) - block.timestamp ) );
-            vestings[_token][msg.sender] = (iv / k - eligibleAmount) * k + (iv % k);
+            vestings[_token][msg.sender] = (iv / k - eligibleAmount) * k + ((iv % k) - ((iv % k) - block.timestamp) );
 
-            s = IERC20(_token).transfer(msg.sender, eligibleAmount);
+
+
+            s = IERC20(_token).transfer(msg.sender, eligibleAmount * oneToken);
             require(s, "Transfer failed");
+
+            emit WithdrewFromVest(_token, msg.sender, eligibleAmount);
+
         }
 
+        
         
         
 
--- src/test/Vestoor.t.sol
+++ src/test/Vestoor.t.sol
@@ -4,7 +4,8 @@ pragma solidity 0.8.11;
 import {DSTestPlus} from "./utils/DSTestPlus.sol";
 import {Vestoor} from "../Vestoor.sol";
 import {MockERC20} from "./utils/MockERC20.sol";
-
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+//import {console2} from "./utils/Console2.sol";
 
 contract VestoorTest is DSTestPlus {
     Vestoor V;
@@ -23,11 +24,63 @@ contract VestoorTest is DSTestPlus {
         E.approve(address(V), type(uint256).max - 1);
     }
 
-    function testSetVest(uint32 _ammount) public {
-        uint256 endD = 22;
-        uint256 ammount = uint256(_ammount) + endD + 1;
-        V.setVest(eAddr, address(1), ammount, endD);
-        bool s = V.getVest(eAddr, address(1)) == (ammount* k) + ( endD * 1 days) ;
+
+    function setupVest(address _beneficiary, uint32 _amt, uint256 _timestampX) public returns (bool) {
+        return V.setVest(eAddr, _beneficiary, oneToken * _amt, _timestampX);
+    }
+
+    function testSetVest(uint32 _ammount_, address _benefits_) public {
+        uint256 endD = 356;
+        vm.assume(_ammount_ > endD);
+        uint256 ammount = uint256(_ammount_);
+
+        vm.assume(_benefits_ != address(0));
+        V.setVest(eAddr, _benefits_, ammount, endD);
+        vm.warp(endD * 2 days);
+        
+        bool s = V.getVest(eAddr, _benefits_) == (ammount* k) + ( endD * 1 days);
         assertTrue(s);
     }
+
+    function testWithdrawAvailable( address _who_, uint32 _amount_, uint8 _timeX_) public {
+        vm.assume(_who_ != address(0));
+        vm.assume(_amount_ > 1);
+        vm.assume(_timeX_ > 1);
+        uint256 t = uint256(_timeX_);
+
+        setupVest(_who_, _amount_, t);
+        uint256 getsVest = V.getVest(eAddr, _who_);
+     
+        bool hasVest = getsVest > _amount_ * k;
+        assertTrue(hasVest);
+        
+        (uint256 v1, uint256 who1) = (IERC20(eAddr).balanceOf(address(V)), IERC20(eAddr).balanceOf(_who_));  
+        assertTrue(who1 == 0);
+        assertTrue(getsVest > _amount_ * k );
+        vm.warp(t * 1 days / 2 + block.timestamp);
+
+        vm.prank(_who_);
+        assertTrue(V.withdrawAvailable(eAddr));
+        assertTrue(IERC20(eAddr).balanceOf(address(V)) >0);
+
+
+        (uint256 v2, uint256 who2) = (IERC20(eAddr).balanceOf(address(V)), IERC20(eAddr).balanceOf(_who_));
+
+        assertTrue(v1 == v2 + who2);
+        assertTrue(getsVest > V.getVest(eAddr, _who_));
+
+        
+        assertTrue(V.getVest(eAddr, _who_) > 0 );
+        vm.warp(t * 1 days / 3 + block.timestamp);
+        assertTrue(V.getVest(eAddr, _who_) > 0 );
+        vm.warp(t * 1 days + block.timestamp);
+        assertTrue(V.getVest(eAddr, _who_) > 0 );
+        vm.prank(_who_);
+        assertTrue(V.withdrawAvailable(eAddr));
+        assertTrue(V.getVest(eAddr, _who_) == 0 );
+
+        /// @todo think edge hasvest -> 0 -> creates vest sme block
+
+    }
+    
 }
