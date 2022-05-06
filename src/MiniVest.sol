6e3aa5db9d9c3bede7e4f3ec2db2716e508a8cda
BogdanA <petra306@protonmail.com>
2022-05-06 19:05:20 +0300

    Update MiniVest.sol
    modifies visibility of withdrawAvailable

--- src/MiniVest.sol
+++ src/MiniVest.sol
@@ -65,7 +65,7 @@ contract MiniVest is ReentrancyGuard {
 
     /// @notice withdraws all tokens that have vested for given ERC20 contract address and msg.sender
     /// @param _token ERC20 contract of token to be withdrawn
-    function withdrawAvailable(address _token) external nonReentrant returns (bool s) {
+    function withdrawAvailable(address _token) public nonReentrant returns (bool s) {
         uint256 iv= vestings[_token][msg.sender];
         require(vestings[_token][msg.sender] != 0, "Nothing to bag");
 
