9730a1a2b9eb63f1be3e0f91781c5d797ac96975
parseb <petra306@protonmail.com>
2022-08-12 17:49:30 +0300

    changes pragma, external public virtual

--- src/MiniVest.sol
+++ src/MiniVest.sol
@@ -42,7 +42,7 @@ contract MiniVest is ReentrancyGuard {
                     address _beneficiary, 
                     uint256 _amount, 
                     uint256 _days) 
-                    internal virtual
+                    public virtual
                     returns (bool s) {
 
         if (vestings[_token][_beneficiary] != 0) revert VestingInProgress(_token, _beneficiary);
@@ -64,7 +64,7 @@ contract MiniVest is ReentrancyGuard {
 
     /// @notice withdraws all tokens that have vested for given ERC20 contract address and msg.sender
     /// @param _token ERC20 contract of token to be withdrawn
-    function withdrawAvailable(address _token) internal virtual nonReentrant returns (bool s) {
+    function withdrawAvailable(address _token) public virtual nonReentrant returns (bool s) {
         uint256 iv= vestings[_token][msg.sender];
         require(vestings[_token][msg.sender] != 0, "Nothing to bag");
 
